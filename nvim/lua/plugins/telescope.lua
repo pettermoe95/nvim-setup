989b2241395d4fa421a0409fa9eabdcfb0760948
pettermoe95 <pettermoe9530@gmail.com>
2024-04-22 10:45:20 +0200

    fzf build change

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,17 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = vim.fn.executable("make") == 1 and "make"
+        or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      enabled = vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1,
+      config = function()
+        LazyVim.on_load("telescope.nvim", function()
+          require("telescope").load_extension("fzf")
+        end)
+      end,
+    },
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
