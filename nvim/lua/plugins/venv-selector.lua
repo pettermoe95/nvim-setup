4bb172811e61d0437b3d7d6ddfd8b67ea4ce1067
petmoe <petter.moe@brilliant.no>
2024-06-26 12:35:39 +0200

    remove some plugins causing problems

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -2,18 +2,16 @@ return {
 	"linux-cultist/venv-selector.nvim",
 	dependencies = {
 		"neovim/nvim-lspconfig",
-		"nvim-telescope/telescope.nvim",
-		"mfussenegger/nvim-dap-python",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
-	opts = {
-		name = { "venv", "env" },
-		auto_refresh = false,
-	},
-	event = "VeryLazy",
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	config = function()
+		require("venv-selector").setup()
+	end,
 	keys = {
-		-- Keymap to open VenvSelector to pick a venv
-		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory)
-		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+		{ ",v", "<cmd>VenvSelect<cr>" },
 	},
 }
