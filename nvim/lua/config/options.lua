e6e56ccc8e1b424e3d355dc42e4593a406b507c9
petmoe <petter.moe@brilliant.no>
2025-03-20 10:18:32 +0100

    update lazy vim setup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 -- Backupdir to prevent backups in git tracking
 vim.cmd([[set backupdir=~/.nvim_backup]])
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.lsp.set_log_level("debug")
