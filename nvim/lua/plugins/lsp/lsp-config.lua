b4231db429e3a9a6a109fa2be0bdc8e9858161b3
petmoe <petter.moe@brilliant.no>
2025-08-14 21:33:58 +0200

    semantic tokens for gopls

--- nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,6 +1,15 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = { enabled = false },
+		servers = {
+			gopls = {
+				settings = {
+					gopls = {
+						semanticTokens = true,
+					},
+				},
+			},
+		},
+	},
 }
